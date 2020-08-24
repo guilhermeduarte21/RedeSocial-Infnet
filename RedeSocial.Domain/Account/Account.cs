499c5ad5d20558896678cf5ea2db87a69164da56
Duarte <guilhermeduartt21@gmail.com>
2020-08-24 14:19:08 -0300

    first commit

---
+++ RedeSocial.Domain/Account/Account.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity;
+
+namespace RedeSocial.Domain.Account
+{
+    public class Account : IdentityUser
+    {
+        public Guid Id { get; set; }
+        public String Name { get; set; }
+        public DateTime DtBirthday { get; set; }
+        public String Email { get; set; }       
+        public String Password { get; set; }
+    }
+}
